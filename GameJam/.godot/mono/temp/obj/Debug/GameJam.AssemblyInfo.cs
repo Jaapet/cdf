6577ba1e7e9552283e2aac24d211ff49df6585ac
FLokyGoon <florestan.bardeau@protonmail.com>
2024-06-30 12:23:36 +0200

    dimanche midi

--- GameJam/.godot/mono/temp/obj/Debug/GameJam.AssemblyInfo.cs
+++ GameJam/.godot/mono/temp/obj/Debug/GameJam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameJam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c401a7449a403c29abace91b875491549a1fade9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42a892b965158f7c9bda7e7ac50f6a5e8faca00")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameJam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameJam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
